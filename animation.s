839d48aa7f7156bd880a4b4351ff94f6acf370c4
Omar Ismail <omarfootball2007@outlook.com>
2026-05-10 13:55:43 +0300

    Revert "changed spi init settings to be common for rfid and animation"
    This reverts commit f1b104c2316873f201f7c87d34aadf8dab0aa03b.

--- animation.s
+++ animation.s
@@ -31,7 +31,6 @@
     IMPORT  pending_dir
 
     ; cross-module functions
-    IMPORT  spi1_common_init
     IMPORT  stopAndServe
     IMPORT  checkNextMove
     
@@ -182,19 +181,11 @@ send_16bit
     BIC R2, #CS_PIN
     STR R2, [R1, #0x0C]
     LDR R1, =SPI1_BASE
-    MOV R3, R0, LSR #8
-    STRB R3, [R1, #0x0C]
+    STR R0, [R1, #0x0C]
 wait_rxne
     LDR R2, [R1, #0x08]
     TST R2, #0x01
     BEQ wait_rxne
-    LDR R2, [R1, #0x0C]
-    UXTB R3, R0
-    STRB R3, [R1, #0x0C]
-wait_rxne_2
-    LDR R2, [R1, #0x08]
-    TST R2, #0x01
-    BEQ wait_rxne_2
     LDR R2, [R1, #0x0C]
 wait_bsy
     LDR R2, [R1, #0x08]
@@ -208,7 +199,11 @@ wait_bsy
 
 matrix_init
     PUSH {LR}
-    BL spi1_common_init
+    LDR R0, =SPI1_BASE
+    LDR R1, =0x0B1C
+    STR R1, [R0, #0x00]
+    ORR R1, #0x0040
+    STR R1, [R0, #0x00]
     MOVW R0, #0x0F00
     BL send_16bit
     MOVW R0, #0x0B07
